6ba13d5a43ae1c2842533d029f7156b823c31cdb
khareRajshree <rajshree.khare@dell.com>
2025-02-07 13:14:22 +0530

    files updated

--- .github/scripts/check_workflow_status.sh
+++ .github/scripts/check_workflow_status.sh
@@ -31,37 +31,30 @@ while true; do
 
   # Poll up to 5 times to check for an in_progress status of the most recently submitted.
   # Once it finds an in_progress workflow, it will keep polling until the workflow is completed successfully or failed.
-  for (( i=0; i<$MAX_RETRIES; i++ )); do
-    if [ "${STATUS}" == "in_progress" ]; then
-      WORKFLOW_ID=$(echo "${RESPONSE}" | jq -r '.workflow_runs[0].id')
-      echo "Workflow ID is: ${WORKFLOW_ID}"
-      echo "Workflow in progress for ${REPO}."
+  if [ "${STATUS}" == "in_progress" ]; then
+    WORKFLOW_ID=$(echo "${RESPONSE}" | jq -r '.workflow_runs[0].id')
+    echo "Workflow ID is: ${WORKFLOW_ID}"
+    echo "Workflow in progress for ${REPO}."
 
-      # Continuously poll until the workflow is completed
-      while [ "${STATUS}" == "in_progress" ]; do
-        sleep "$POLL_INTERVAL"
-        RESPONSE=$(curl -s -H "Authorization: token $GITHUB_TOKEN" "$API_URL")
-        STATUS=$(echo "${RESPONSE}" | jq -r '.workflow_runs[0].status')
-        CONCLUSION=$(echo "${RESPONSE}" | jq -r '.workflow_runs[0].conclusion')
-      done
-
-      if [ "${STATUS}" == "completed" ]; then
-        if [ "${CONCLUSION}" == "success" ]; then
-          echo "Workflow completed successfully for ${REPO}."
-          exit 0
-        else
-          echo "Workflow failed for ${REPO}."
-          exit 1
-        fi
-      fi
-    else
-      echo "No in-progress workflow found for ${REPO}. Waiting for $POLL_INTERVAL seconds..."
+    while [ "${STATUS}" == "in_progress" ]; do
       sleep "$POLL_INTERVAL"
-      RESPONSE=$(curl -s -H "Authorization: token $GITHUB_TOKEN" "$API_URL")
+      RESPONSE=$(curl -s -H "Authorization: token $GITHUB_TOKEN" "https://api.github.com/repos/${REPO}/actions/runs?event=repository_dispatch")
       STATUS=$(echo "${RESPONSE}" | jq -r '.workflow_runs[0].status')
-    fi
-  done
+      CONCLUSION=$(echo "${RESPONSE}" | jq -r '.workflow_runs[0].conclusion')
+    done
 
-  echo "Maximum retries exhausted, no recent workflow submitted for ${REPO}."
-  exit 0
+    if [ "${STATUS}" == "completed" ]; then
+      if [ "${CONCLUSION}" == "success" ]; then
+        echo "Workflow completed successfully for ${REPO}."
+        exit 0
+      else
+        echo "Workflow failed for ${REPO}."
+        exit 1
+      fi
+    fi
+  else
+    echo "No in-progress workflow found for ${REPO}. Waiting for $POLL_INTERVAL seconds..."
+  fi
+
+  sleep "$POLL_INTERVAL"
 done
