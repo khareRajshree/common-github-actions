5f5715b3c6af248755a09fdd6fc6007e9674449e
khareRajshree <rajshree.khare@dell.com>
2025-02-07 15:43:12 +0530

    files updated

--- .github/scripts/check_workflow_status.sh
+++ .github/scripts/check_workflow_status.sh
@@ -7,6 +7,7 @@ MAX_RETRIES=5
 POLL_INTERVAL=60
 RETRY_COUNT=0
 API_URL="https://api.github.com/repos/${REPO}/actions/runs?event=${EVENT_TYPE}"
+latest_workflow_id=""
 
 echo "Checking workflow status for ${REPO}..."
 echo "URL: ${API_URL}"
@@ -28,48 +29,40 @@ while true; do
     fi
   fi
 
-  # Get the current time
-  current_time=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
+  STATUS=$(echo "${RESPONSE}" | jq -r '.workflow_runs[0].status')
+  CONCLUSION=$(echo "${RESPONSE}" | jq -r '.workflow_runs[0].conclusion')
+  WORKFLOW_ID=$(echo "${RESPONSE}" | jq -r '.workflow_runs[0].id')
 
-  # Find the most recent workflow run based on the timestamp
-  latest_run=$(echo "${RESPONSE}" | jq '.workflow_runs | sort_by(.created_at) | last')
+  # Check if the workflow ID is different from the last detected workflow ID
+  if [ "$WORKFLOW_ID" != "$LATEST_WORKFLOW_ID" ]; then
+    LATEST_WORKFLOW_ID="$WORKFLOW_ID"
+    echo "New workflow detected. ID: ${WORKFLOW_ID}"
+    echo "verify if its from the same parent dispatch event and update the workflow ID"
+  fi
 
-  # Extract relevant information
-  created_at=$(echo "${latest_run}" | jq -r '.created_at')
-  status=$(echo "${latest_run}" | jq -r '.status')
-  conclusion=$(echo "${latest_run}" | jq -r '.conclusion')
+  # Poll up to 5 times to check for an in_progress status of the most recently submitted.
+  # Once it finds an in_progress workflow, it will keep polling until the workflow is completed successfully or failed.
+  if [ "${STATUS}" == "in_progress" ]; then
+    echo "Workflow in progress for ${REPO}."
 
-  # Compare the created_at timestamp with the current time
-  if [[ "$created_at" < "$current_time" ]]; then
-    # Poll up to 5 times to check for an in_progress status of the most recently submitted.
-    # Once it finds an in_progress workflow, it will keep polling until the workflow is completed successfully or failed.
-    if [ "${status}" == "in_progress" ]; then
-      workflow_id=$(echo "${latest_run}" | jq -r '.id')
-      echo "Workflow ID is: ${workflow_id}"
-      echo "Workflow in progress for ${REPO}."
+    while [ "${STATUS}" == "in_progress" ]; do
+      sleep "$POLL_INTERVAL"
+      RESPONSE=$(curl -s -H "Authorization: token $GITHUB_TOKEN" "$API_URL")
+      STATUS=$(echo "${RESPONSE}" | jq -r '.workflow_runs[0].status')
+      CONCLUSION=$(echo "${RESPONSE}" | jq -r '.workflow_runs[0].conclusion')
+    done
 
-      while [ "${status}" == "in_progress" ]; do
-        sleep "$POLL_INTERVAL"
-        RESPONSE=$(curl -s -H "Authorization: token $GITHUB_TOKEN" "$API_URL")
-        latest_run=$(echo "${RESPONSE}" | jq '.workflow_runs | sort_by(.created_at) | last')
-        status=$(echo "${latest_run}" | jq -r '.status')
-        conclusion=$(echo "${latest_run}" | jq -r '.conclusion')
-      done
-
-      if [ "${status}" == "completed" ]; then
-        if [ "${conclusion}" == "success" ]; then
-          echo "Workflow completed successfully for ${REPO}."
-          exit 0
-        else
-          echo "Workflow failed for ${REPO}."
-          exit 1
-        fi
+    if [ "${STATUS}" == "completed" ]; then
+      if [ "${CONCLUSION}" == "success" ]; then
+        echo "Workflow completed successfully for ${REPO}."
+        exit 0
+      else
+        echo "Workflow failed for ${REPO}."
+        exit 1
       fi
-    else
-      echo "No in-progress workflow found for ${REPO}. Waiting for $POLL_INTERVAL seconds..."
     fi
   else
-    echo "No recent workflow runs found."
+    echo "No in-progress workflow found for ${REPO}. Waiting for $POLL_INTERVAL seconds..."
   fi
 
   sleep "$POLL_INTERVAL"
